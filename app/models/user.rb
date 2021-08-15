54ded4a5496ce89eb9614eb1f1071570a6d22d1a
Rommy Gustiawan <rgcppp@gmail.com>
2021-08-15 14:57:19 +0700

    Add if statement at result[:valid] = false
    result[:valid] is set to false only when the current value is true, so
    we don't change it when the value is already false
    
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,32 +12,32 @@ class User
     }
 
     if @username.nil? || @username.empty?
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Username can't be nil or empty."
     end
 
     if !@username.nil? && @username.size > 20
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Username can't be more than 20 characters."
     end
 
     if @email.nil? || @email.empty?
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Email can't be nil or empty."
     end
 
     if !@email.nil? && @email.size > 50
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Email can't be more than 50 characters."
     end
 
     if @bio.nil? || @bio.empty?
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Bio can't be nil or empty."
     end
 
     if !@bio.nil? && @bio.size > 200
-      result[:valid] = false
+      result[:valid] = false if result[:valid]
       result[:errors] << "Bio can't be more than 200 characters."
     end
 
