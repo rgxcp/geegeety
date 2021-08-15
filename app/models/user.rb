6e98797be2533570d3d576c25af2dc757df742b5
Rommy Gustiawan <rgcppp@gmail.com>
2021-08-15 14:31:35 +0700

    User#validate failed when uname.size > 20
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,21 @@ class User
   end
 
   def validate
-    {
-      :valid => false,
-      :errors => ["Username can't be nil or empty."]
+    result = {
+      :valid => true,
+      :errors => []
     }
+
+    if @username.nil? || @username.empty?
+      result[:valid] = false
+      result[:errors] << "Username can't be nil or empty."
+    end
+
+    if !@username.nil? && @username.size > 20
+      result[:valid] = false
+      result[:errors] << "Username can't be more than 20 characters."
+    end
+
+    result
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,16 @@ describe User do
         expect(validate_result[:errors].first).to eq("Username can't be nil or empty.")
       end
     end
+
+    context "when username characters more than 20" do
+      it "will return false with error" do
+        user = User.new({
+          :username => "johndoe" * 3
+        })
+        validate_result = user.validate
+        expect(validate_result[:valid]).to be_falsey
+        expect(validate_result[:errors].first).to eq("Username can't be more than 20 characters.")
+      end
+    end
   end
 end
