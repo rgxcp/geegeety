10a8c3d93b67bc35cf57160efcf3c8a62b71dcc9
Rommy Gustiawan <rgcppp@gmail.com>
2021-08-17 14:46:01 +0700

    Hashtag#validate failed when hashtagable_type nil
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,12 +1,25 @@
 class Hashtag
   def initialize(params)
     @hashtagable_id = params[:hashtagable_id]
+    @hashtagable_type = params[:hashtagable_type]
   end
 
   def validate
-    {
-      :valid => false,
-      :errors => ["Hashtagable Id can't be nil or empty."]
+    result = {
+      :valid => true,
+      :errors => []
     }
+
+    if @hashtagable_id.nil?
+      result[:valid] = false
+      result[:errors] << "Hashtagable Id can't be nil or empty."
+    end
+
+    if @hashtagable_type.nil? || @hashtagable_type.empty?
+      result[:valid] = false if result[:valid]
+      result[:errors] << "Hashtagable Type can't be nil or empty."
+    end
+
+    result
   end
 end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -12,5 +12,25 @@ describe Hashtag do
         expect(validate_result[:errors].first).to eq("Hashtagable Id can't be nil or empty.")
       end
     end
+
+    context "when hashtagable_type nil or empty" do
+      it "will return falsey hash with errors" do
+        hashtag = Hashtag.new({
+          :hashtagable_id => 1,
+          :hashtagable_type => nil
+        })
+        validate_result = hashtag.validate
+        expect(validate_result[:valid]).to be_falsey
+        expect(validate_result[:errors].first).to eq("Hashtagable Type can't be nil or empty.")
+
+        hashtag = Hashtag.new({
+          :hashtagable_id => 1,
+          :hashtagable_type => ""
+        })
+        validate_result = hashtag.validate
+        expect(validate_result[:valid]).to be_falsey
+        expect(validate_result[:errors].first).to eq("Hashtagable Type can't be nil or empty.")
+      end
+    end
   end
 end
