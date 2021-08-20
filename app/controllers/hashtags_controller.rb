d0bb3b2279d8a796b4a759a2f12f0b887c34a286
Rommy Gustiawan <rgcppp@gmail.com>
2021-08-21 01:05:21 +0700

    GET "/api/v1/hashtags/trending" return 200
    with hashtags data
    
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

---
+++ app/controllers/hashtags_controller.rb
@@ -0,0 +1,17 @@
+require_relative "application_controller"
+require_relative "../models/hashtag"
+
+class HashtagsController < ApplicationController
+  get "/api/v1/hashtags/trending" do
+    hashtags = Hashtag.trending
+
+    status 200
+    result = {
+      :status => "Success",
+      :data => {
+        :hashtags => hashtags
+      }
+    }
+    result.to_json
+  end
+end
---
+++ spec/controllers/hashtags_controller_spec.rb
@@ -0,0 +1,32 @@
+ENV["APP_ENV"] = "test"
+
+require_relative "../../app/controllers/hashtags_controller"
+
+describe HashtagsController do
+  def app
+    HashtagsController
+  end
+
+  it "will return 200 with hashtags data" do
+    allow(Hashtag)
+      .to receive(:trending)
+      .and_return([{
+        :name => "backend",
+        :count => 21
+      }])
+
+    get "/api/v1/hashtags/trending"
+
+    expect(last_response).to be_ok
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to eq({
+      :status => "Success",
+      :data => {
+        :hashtags => [{
+          :name => "backend",
+          :count => 21
+        }]
+      }
+    }.to_json)
+  end
+end
